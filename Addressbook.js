0204b39506c00cd018ce104924b050685052fc65
moin21 <mamoinuddin@gmail.com>
2022-04-27 05:46:20 +0530

    Added: Logic to Validate Fields with regex

--- Addressbook.js
+++ Addressbook.js
@@ -1,14 +1,24 @@
+/**
+ * Regex pattern for the data
+ */
+const nameRegex = RegExp("^[A-Z]{1}[a-z]{2,}$");
+const addressRegex = RegExp("^[a-zA-z0-9#,]{4,}$");
+const cityStateRegex = RegExp("^[a-zA-z]{4,}$");
+const zipRegex = RegExp("^[0-9]{3}\\s{0,1}[0-9]{3}$");
+const phoneNumberRegex = RegExp("^[0-9]{2}[-]{1}[0-9]{10}$");
+const emailRegex = RegExp(
+  "^[a-zA-Z]+[a-zA-Z0-9]*[- . + _]?[a-zA-Z0-9]+[@]{1}[a-z0-9]+[.]{1}[a-z]+[.]?[a-z]+$"
+);
+
+/**
+ * Class contact where we have defined the constructor and the getter and setter
+ * We are setting the value if the regex pattern matches.
+ */
 class Contact {
   /**
-   *
+   * Constructor
+   * @param  {...any} param - above mentioned fields.
    */
-  firstName;
-  lastName;
-  city;
-  state;
-  phoneNumber;
-  email;
-
   constructor(...params) {
     this.firstName = params[0];
     this.lastName = params[1];
@@ -18,6 +28,9 @@ class Contact {
     this.phoneNumber = params[5];
     this.email = params[6];
   }
+  /**
+   * Getters for all fields
+   */
   get firstName() {
     return this._firstName;
   }
@@ -45,35 +58,52 @@ class Contact {
   get email() {
     return this._email;
   }
-
+  /**
+   * Setters for all fields
+   */
   set firstName(firstName) {
-    this._firstName = firstName;
+    if (nameRegex.test(firstName)) this._firstName = firstName;
+    else throw "**** FIRST NAME is Incorrect ****";
   }
 
   set lastName(lastName) {
-    this._lastName = lastName;
+    if (nameRegex.test(lastName)) this._lastName = lastName;
+    else throw "**** LAST NAME is Incorrect ****";
+  }
+
+  set address(address) {
+    if (addressRegex.test(address)) this._address = address;
+    else throw "**** ADDRESS is Incorrect ****";
   }
 
   set city(city) {
-    this._city = city;
+    if (cityStateRegex.test(city)) this._city = city;
+    else throw "**** CITY is Incorrect ****";
   }
 
   set state(state) {
-    this._state = state;
+    if (cityStateRegex.test(state)) this._state = state;
+    else throw "**** STATE is Incorrect ****";
   }
 
   set zip(zip) {
-    this._zip = zip;
+    if (zipRegex.test(zip)) this._zip = zip;
+    else throw "**** ZIP is Incorrect ****";
   }
 
   set phoneNumber(phoneNumber) {
-    this._phoneNumber = phoneNumber;
+    if (phoneNumberRegex.test(phoneNumber)) this._phoneNumber = phoneNumber;
+    else throw "**** PHONE NUMBER is Incorrect ****";
   }
 
   set email(email) {
-    this._email = email;
+    if (emailRegex.test(email)) this._email = email;
+    else throw "**** EMAIL ADDRESS is Incorrect ****";
   }
-
+  /**
+   * To String Method
+   * @returns - Contact object in String form.
+   */
   toString() {
     return (
       "First Name : " +
@@ -93,14 +123,16 @@ class Contact {
     );
   }
 }
-
+/**
+ * Creating new contact object.
+ */
 let contact = new Contact(
   "Moinuddin",
   "Asraf",
   "Jaipur",
   "Rajasthan",
   311001,
-  99999999,
+  "91-9999999999",
   "moin@gmail.com"
 );
 console.log(contact.toString());
